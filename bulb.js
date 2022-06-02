baa23e87ce5a9cc299883080a99b12ee224a101f
qudus2019 <103913778+qudus2019@users.noreply.github.com>
2022-06-02 14:56:45 +0100

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,9 @@
+function turnOn(){
+    document.getElementById("bulb").className="turnon";
+    document.getElementById("body").className="turnofff";
+}
+
+function turnOff(){
+    document.getElementById("bulb").className="";
+    document.getElementById("body").className="";
+}
